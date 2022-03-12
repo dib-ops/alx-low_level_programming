53c31e9d29ceef6e972b8f68e333134286225525
dib-ops <fayemathias0@gmail.com>
2022-03-11 19:44:18 -0800

    C pour les débutants

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-	putchar(n);
+		putchar(n);
 	}
-for (ch = 'a'; ch <= 'f'; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
