cc56f02cf8bee9fda623b4aa551eb9e2420ae4e6
dib-ops <fayemathias0@gmail.com>
2022-03-13 15:58:34 -0700

    C pour les debutants

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly "Programming is like building a multilingal puzzle"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingal puzzle");
+	return (0);
+}
