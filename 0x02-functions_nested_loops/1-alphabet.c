7688c315b958be01710360d76f8b5a9c2289efaa
dib-ops <fayemathias0@gmail.com>
2022-03-16 14:41:14 -0700

    C pour les débutants: functions nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		_putchar('a - z');
+		_putchar('\n');
+
+		retun (0);
+	}
+}
