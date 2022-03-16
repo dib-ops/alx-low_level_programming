bb60e997f2850523ef041f2d0d9f4adc7976fd96
dib-ops <fayemathias0@gmail.com>
2022-03-16 15:54:26 -0700

    C pour les débutants: functions nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int n;
+
+	ch = "a";
+	n = 0;
+
+	while (ch <= "z" && n <= 10)
+	{
+		_putchar(ch);
+			ch++;
+			n++;
+	}
+
+	_putchar("\n");
+}
