d7395570c3491ba881f857198da18bb08f88aa77
dib-ops <fayemathias0@gmail.com>
2022-03-15 17:27:47 -0700

    C pour les débutants: functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - prints _putchar,
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
