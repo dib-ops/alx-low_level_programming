199cc1017047e7453376754d8cff3f1dfed1f062
dib-ops <fayemathias0@gmail.com>
2022-03-22 00:40:19 -0700

    Apprendre à déboguer son code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,10 +6,17 @@
  * main - tests function that prints if integer is positive or negative
  * Return: Always 0 (Success)
  */
-void positive_or_negative(int i)
+void positive_or_negative(int n)
 {
-	int i;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
