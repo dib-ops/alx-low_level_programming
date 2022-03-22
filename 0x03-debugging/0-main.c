9a50f8b6cd7a300afcd61e554a700337344ee4bd
dib-ops <fayemathias0@gmail.com>
2022-03-22 00:26:02 -0700

    Apprendre à déboguer son code

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	int i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
