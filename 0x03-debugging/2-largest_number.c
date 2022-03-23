10b9a1d7538223f48b1d01cac68a6c23f5fbd340
dib-ops <fayemathias0@gmail.com>
2022-03-22 17:12:31 -0700

    Apprendre à déboguer son code

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,16 +15,39 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
-		printf("%d is the largest number\n", largest);
-	} else if (b > a && b > c)
+	}
+	else if (a > b && b < c)
+	{
+		if (a > c)
+			largest = a;
+		else
+			largest = c
+	}
+	else if (b > a && a > c)
 	{
 		largest = b;
+		else if (b > a && a < c)
+		{
+			if (b > c)
+				largest = b;
+			else
+				largest = c;
+		}
+			
 		printf("%d is the largest number\n", largest);
+	}
+	else if (a == b && a == c)
+	{
+		largest = a;
 	}
 	else
 	{
-		largest = c;
-		printf("%d is the largest number\n", largest);
+		if ((a == b && a > c) || (a == c && a > b) || (b == c && a > b))
+			largest = a;
+		else if ((a == c && a < b) || (b == c && a < b))
+			largest = b;
+		else
+			largest = c;
 	}
 
 	return (largest);
