3a77c4e6db35debfd2f6daac6ca7133e3f0bd39b
dib-ops <fayemathias0@gmail.com>
2022-04-02 00:00:34 -0700

    Check for a digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,14 @@
 #define FILE_MAIN
 
 int _isupper(int c);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonale(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
 
 #endif
