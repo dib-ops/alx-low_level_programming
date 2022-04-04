a6f4f066378c44989a6dd16ad6a73c76a4861cf4
dib-ops <fayemathias0@gmail.com>
2022-04-04 16:18:43 -0700

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a = 0;
+
+	if (a < 10)
+	{
+		_putchar('a');
+		_putchar('\n');
+	}
+
+	return (0);
+}
