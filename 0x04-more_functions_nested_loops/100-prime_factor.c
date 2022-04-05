9605b92bc4d00f9564c391027bce10425305691e
dib-ops <fayemathias0@gmail.com>
2022-04-04 23:54:30 -0700

    More functions nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 	long int n;
-       	long int max;
+	long int max;
 	long int i;
 
 	n = 612852475143;
 	max = -1;
 
-	while ( n % 2 == 0)
+	while (n % 2 == 0)
 	{
 		max = 2;
 		n /= 2;
