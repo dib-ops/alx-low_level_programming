651ee0dd180074b98b12aac6d4a0b55c135c933a
dib-ops <fayemathias0@gmail.com>
2022-04-05 13:24:18 -0700

    More functions nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,10 +16,10 @@ void print_square(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-				{
-					_putchar('#');
-				}
-				_putchar('\n');
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 	}
 }
