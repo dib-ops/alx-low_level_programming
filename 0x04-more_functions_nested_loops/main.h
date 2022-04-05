f1f4806b4e26363e3372a5096f4c3a622a6f3d76
dib-ops <fayemathias0@gmail.com>
2022-04-05 11:51:17 -0700

    More functions nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _isupper(int c);
 void print_triangle(int size);
 void print_square(int size);
-void print_diagonale(int n);
+void print_diagonal(int n);
 void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
