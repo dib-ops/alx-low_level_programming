b3704c292eb4e15f6b46277cb5cf7c188a758c12
dib-ops <fayemathias0@gmail.com>
2022-04-25 19:35:27 -0700

    Create 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,18 +3,18 @@
 /**
  * add_nodeint - adds a node to the beginning of a linked list
  * @head: pointer to the head of the list
- * @i: integer to be used as content
+ * @n: integer to be used as content
  *
  * Return: address of the newly added node
  */
-listint_t *add_nodeint(listint_t **head, const int i)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node != NULL)
 	{
-		new_node->i = i;
+		new_node->n = n;
 		new_node->next = *head;
 	}
 	else
