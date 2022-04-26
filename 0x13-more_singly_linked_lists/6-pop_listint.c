df97bef0435aa657391939197c470c4218032050
dib-ops <fayemathias0@gmail.com>
2022-04-25 19:46:16 -0700

    Create 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- *pop_listint - deletes the head node of a linked list
- *@head: head of the list
+ * pop_listint - deletes the head node of a linked list
+ * @head: head of the list
  *
- *Return: the deleted node data
+ * Return: the deleted node data
  */
-
 int pop_listint(listint_t **head)
 {
 	listint_t *popped;
