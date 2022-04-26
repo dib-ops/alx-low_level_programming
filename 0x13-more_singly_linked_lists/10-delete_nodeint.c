c2f32fe614b0506812c848044de60ff9a5f921bb
dib-ops <fayemathias0@gmail.com>
2022-04-26 02:19:16 +0000

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -37,22 +37,3 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	return (-1);
 }
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
-}
