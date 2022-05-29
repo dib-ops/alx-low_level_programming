4b840970e23b209536d97ee7c7dd3c47a7150718
dib-ops <fayemathias0@gmail.com>
2022-05-29 23:57:18 +0000

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,19 @@
+#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
-
+#include <limits.h>
 /**
- * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- *
- * Return: a pointer to the allocated memory
+ * malloc_checked - creates an array of integers.
+ * @b: input.
+ * Return: Output.
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *g = malloc(b);
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	if (g == NULL)
+	{
 		exit(98);
-
-	return (ptr);
+	}
+	return (g);
 }
