521380b48bdaa7f1504599b360b9ab058535e6dd
dib-ops <fayemathias0@gmail.com>
2022-05-30 00:00:26 +0000

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,46 +1,27 @@
+#include "main.h"
 #include <stdlib.h>
-#include "holberton.h"
-
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (s);
-}
-
-/**
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
- *
- * Return: pointer to allocated memory
- */
+* _calloc - prints calloc;
+* @nmemb: input string.
+* @size: input string.
+* Return: Pointer.
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	char *p;
+	unsigned int i, d;
 
-	if (nmemb == 0 || size == 0)
+	d = nmemb * size;
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
-	ptr = malloc(size * nmemb);
-
-	if (ptr == NULL)
+	p = malloc(d);
+	if (p == NULL)
 		return (NULL);
 
-	_memset(ptr, 0, nmemb * size);
-
-	return (ptr);
+	for (i = 0; i < (d); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
 }
